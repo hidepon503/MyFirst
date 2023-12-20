fe559e6638d3a9e0e242892ef617af03f95f9393
hidepon503 <blue.passion625@gmail.com>
2023-12-20 13:31:19 +0900

    Initial Commit

---
+++ MyFirst/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyFirst
+//
+//  Created by 飯塚 秀幸 on 2023/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by 飯塚 秀幸 on 2023/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
